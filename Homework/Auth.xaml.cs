e3213c232a612ffbb3909423ba64a6e149f14711
Леонид Петров <lapshao@vk.com>
2024-01-24 02:11:45 +0300

    Добавьте файлы проекта.

---
+++ Homework/Auth.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Homework.Helper;
+
+namespace Homework
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : ContentControl
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, RoutedEventArgs e)
+        {
+            var user = ClassDB.entity.User.Where(i => i.Login == txbLogin.Text && i.Password == psbPassword.Password).FirstOrDefault();
+
+            if (user.IdRole == 1)
+            {
+                MessageBox.Show("You enter to the system as Administator");
+            }
+        }
+    }
+}
---
+++ Homework/Helper/ClassDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.Helper
+{
+    internal class ClassDB
+    {
+        public static LoginFramesEntities entity = new LoginFramesEntities();
+    }
+}
